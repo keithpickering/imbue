fe74e45a5d7c2d0da4c9cf229a84227bc48325d7
Keith Pickering <keith@kmgsoftware.com>
2018-05-08 09:21:40 -0700

    Little tweaks, make mobile nav work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+const Page = {
+	settings: {
+	},
+
+	init: function(el) {
+		const _this = this;
+
+		// Mobile menu toggle
+		const page_head = document.getElementById("page-head");
+		const menu_toggle = document.getElementById("page-menu-toggle");
+		const menu = document.getElementById("page-menu");
+
+		menu_toggle.addEventListener("click", function(e) {
+			e.preventDefault();
+
+			menu.classList.toggle("is-active");
+		});
+	}
+};
+
+(function() {
+	Page.init();
+})();
 const Accordion = {
 	settings: {
 		// Expand the first item by default
